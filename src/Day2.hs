c28937ea2bbf138b135fe6d5bd7cf120ae85522a
joshcbrown <80245312+joshcbrown@users.noreply.github.com>
2023-12-02 21:03:24 +0100

    clean

--- src/Day2.hs
+++ src/Day2.hs
@@ -13,6 +13,9 @@ import qualified Data.Text as T
 data Balls = Blue Int | Red Int | Green Int
     deriving (Show)
 type Round = [Balls]
+
+-- realised after the fact that really i should be parsing [Bag] rather than [Balls],
+-- but cbs changin it
 data Bag = Bag {red :: Int, blue :: Int, green :: Int}
     deriving (Show)
 data Game = Game {game_id :: Int, rounds :: [Round]}
@@ -31,14 +34,18 @@ pRound =
             ]
             <* optional (string ", ")
 
-pRounds :: Parser Game
-pRounds = Game <$> gid <*> game_rounds
+game :: Parser Game
+game = Game <$> gid <*> game_rounds
   where
     gid = string "Game " *> decimal <* string ": "
     game_rounds = many $ pRound <* (void (string "; ") <|> endOfLine)
 
-pPenis :: Parser [Game]
-pPenis = many pRounds <* endOfInput
+runPartWith :: ([Game] -> String) -> String -> IO ()
+runPartWith f =
+    putStrLn
+        . either id f
+        . parseOnly (many game <* endOfInput)
+        . T.pack
 
 validate :: Game -> Bool
 validate = all (all ok) . rounds
@@ -47,23 +54,21 @@ validate = all (all ok) . rounds
     ok (Green n) = n <= 13
     ok (Blue n) = n <= 14
 
-runPartWith :: ([Game] -> String) -> String -> IO ()
-runPartWith f =
-    putStrLn
-        . either id f
-        . parseOnly pPenis
-        . T.pack
-
 part1 :: String -> IO ()
 part1 = runPartWith (show . sum . map game_id . filter validate)
-
-foldFunc :: Bag -> Balls -> Bag
-foldFunc acc (Blue n) = acc{blue = max n (blue acc)}
-foldFunc acc (Red n) = acc{red = max n (red acc)}
-foldFunc acc (Green n) = acc{green = max n (green acc)}
 
 power :: Bag -> Int
 power b = red b * blue b * green b
 
 part2 :: String -> IO ()
-part2 = runPartWith (show . sum . map (power . foldl foldFunc Bag{red = 0, green = 0, blue = 0} . concat . rounds))
+part2 =
+    runPartWith
+        ( show
+            . sum
+            . map (power . minBag . concat . rounds)
+        )
+  where
+    minBag = foldl foldFunc Bag{red = 0, green = 0, blue = 0}
+    foldFunc acc (Blue n) = acc{blue = max n (blue acc)}
+    foldFunc acc (Red n) = acc{red = max n (red acc)}
+    foldFunc acc (Green n) = acc{green = max n (green acc)}
